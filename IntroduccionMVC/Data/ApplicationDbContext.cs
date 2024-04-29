299eb3d9db02f01f6b2287307196dd19a500dad3
federico almaraz <federicoalmaraz37@gmail.com>
2024-04-29 19:36:50 -0300

    Agregar archivos de proyecto.

---
+++ IntroduccionMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using IntroduccionMVC.Data.Entities;
+using IntroduccionMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-AJI5OKL\\SQLEXPRESS;Initial Catalog=aspnet-IntroduccionMVC-E925FED6-ECB3-4836-977B-02C4DB56EBAF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=true;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+    }
+}
